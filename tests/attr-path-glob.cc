4bc02baa14c3c7896f96b51c2fcbe00633df7312
Alex Ameen <alex.ameen.tx@gmail.com>
2023-06-12 09:15:45 -0500

    test for globSystem with a list of prefixes

--- tests/attr-path-glob.cc
+++ tests/attr-path-glob.cc
@@ -9,12 +9,24 @@
 #include <nlohmann/json.hpp>
 #include <vector>
 #include "flox/types.hh"
+#include "flox/util.hh"
 #include <functional>
+#include <nix/eval-inline.hh>
+#include <nix/eval.hh>
 
 
 /* -------------------------------------------------------------------------- */
 
 using namespace flox::resolve;
+
+/* -------------------------------------------------------------------------- */
+
+static const std::string nixpkgsRef =
+  "github:NixOS/nixpkgs/e8039594435c68eb4f780f3e9bf3972a7399c4b1";
+
+static const std::string flocoPkgsRef =
+  "github:aakropotkin/flocoPackages/2afd962bbd6745d4d101c2924de34c5326042928";
+
 
 /* -------------------------------------------------------------------------- */
 
@@ -127,10 +139,77 @@ test_coerceRelative1()
 
 /* -------------------------------------------------------------------------- */
 
+/* Check that `globSystems' helper returns one cursor for each system. */
+  bool
+test_globSystems1( nix::EvalState & state )
+{
+  FloxFlakeRef                         ref   = coerceFlakeRef( nixpkgsRef );
+  nix::ref<nix::eval_cache::EvalCache> cache = coerceEvalCache( state, ref );
+
+  std::vector<nix::Symbol> path = { state.symbols.create( "legacyPackages" ) };
+  Cursor                   cur  = cache->getRoot()->getAttr( path[0] );
+  CursorPos                c    = std::make_pair( cur, path );
+
+  std::vector<CursorPos> sysPkgs = globSystems( state, c );
+
+  return
+    ( sysPkgs.size() == defaultSystems.size() ) &&
+    ( state.symbols[sysPkgs[0].second[1]] == ( * defaultSystems.cbegin() ) );
+}
+
+
+/* -------------------------------------------------------------------------- */
+
+/* Check that `globSystems' helper returns one cursor for each system for
+ * multiple prefixes. */
+  bool
+test_globSystems2( nix::EvalState & state )
+{
+  FloxFlakeRef                         ref   = coerceFlakeRef( flocoPkgsRef );
+  nix::ref<nix::eval_cache::EvalCache> cache = coerceEvalCache( state, ref );
+
+  std::vector<nix::Symbol> path = { state.symbols.create( "legacyPackages" ) };
+  Cursor                   cur  = cache->getRoot()->getAttr( path[0] );
+  CursorPos                c    = std::make_pair( cur, path );
+
+  std::vector<CursorPos> roots = { c };
+
+  path[0] = state.symbols.create( "packages" );
+  cur     = cache->getRoot()->getAttr( path[0] );
+  c       = std::make_pair( cur, path );
+  roots.push_back( c );
+
+  std::vector<CursorPos> sysPkgs = globSystems( state, roots );
+
+  return
+    ( sysPkgs.size() == ( 2 * defaultSystems.size() ) ) &&
+    ( state.symbols[sysPkgs[0].second[1]] == ( * defaultSystems.cbegin() ) ) &&
+    ( state.symbols[sysPkgs[4].second[1]] == ( * defaultSystems.cbegin() ) );
+}
+
+
+/* -------------------------------------------------------------------------- */
+
 #define RUN_TEST( _NAME )                                              \
   try                                                                  \
     {                                                                  \
       if ( ! test_ ## _NAME () )                                       \
+        {                                                              \
+          ec = EXIT_FAILURE;                                           \
+          std::cerr << "  fail: " # _NAME << std::endl;                \
+        }                                                              \
+    }                                                                  \
+  catch( std::exception & e )                                          \
+    {                                                                  \
+      ec = EXIT_FAILURE;                                               \
+      std::cerr << "  ERROR: " # _NAME ": " << e.what() << std::endl;  \
+    }
+
+
+#define RUN_TEST_WITH_STATE( _STATE, _NAME )                           \
+  try                                                                  \
+    {                                                                  \
+      if ( ! test_ ## _NAME ( _STATE ) )                               \
         {                                                              \
           ec = EXIT_FAILURE;                                           \
           std::cerr << "  fail: " # _NAME << std::endl;                \
@@ -158,6 +237,14 @@ main( int argc, char * argv[], char ** envp )
   RUN_TEST( isAbsAttrPath5 );
   RUN_TEST( isAbsAttrPathJSON1 );
   RUN_TEST( coerceRelative1 );
+
+  nix::initNix();
+  nix::initGC();
+  nix::evalSettings.pureEval = true;  /* Our reference is locked so we can. */
+  nix::EvalState state( {}, nix::openStore() );
+
+  RUN_TEST_WITH_STATE( state, globSystems1 );
+  RUN_TEST_WITH_STATE( state, globSystems2 );
 
   return ec;
 }
