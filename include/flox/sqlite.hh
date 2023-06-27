a03ea8da8ab106dc7cff41a3e248f4158ab9fb18
Alex Ameen <alex.ameen.tx@gmail.com>
2023-06-27 09:44:18 -0500

    extend nix's SQLite to allow flags

---
+++ include/flox/sqlite.hh
@@ -0,0 +1,64 @@
+/* ========================================================================== *
+ *
+ *
+ *
+ * -------------------------------------------------------------------------- */
+
+#pragma once
+
+#include <nix/globals.hh>
+#include <nix/sqlite.hh>
+#include <sqlite3.h>
+
+
+/* -------------------------------------------------------------------------- */
+
+namespace flox {
+  namespace sqlite {
+
+/* -------------------------------------------------------------------------- */
+
+class SQLiteDb : public nix::SQLite {
+
+  public:
+    SQLiteDb(
+      const std::string & path
+    ,       int           flags = SQLITE_OPEN_READWRITE | SQLITE_OPEN_CREATE
+    ,       bool          cache = false
+    ,       bool          trace = false
+    , const char        * vfs   = nix::settings.useSQLiteWAL ? nullptr
+                                                             : "unix-dotfile"
+    );
+
+    SQLiteDb(
+      const std::string & path
+    ,       bool   create = true
+    ,       bool   write  = true
+    ,       bool   cache  = false
+    ,       bool   trace  = false
+    , const char * vfs    = nix::settings.useSQLiteWAL ? nullptr
+                                                       : "unix-dotfile"
+    ) : SQLiteDb( path
+                , ( ( create ? SQLITE_OPEN_CREATE : 0 ) |
+                    ( write  ? SQLITE_OPEN_READWRITE : SQLITE_OPEN_READONLY )
+                  )
+                , cache
+                , trace
+                , vfs
+                )
+    {}
+
+};  /* End class `SQLiteDb' */
+
+
+/* -------------------------------------------------------------------------- */
+
+  }  /* End Namespace `flox::sqlite' */
+}  /* End Namespace `flox' */
+
+
+/* -------------------------------------------------------------------------- *
+ *
+ *
+ *
+ * ========================================================================== */
---
+++ src/sqlite.cc
@@ -0,0 +1,69 @@
+/* ========================================================================== *
+ *
+ *
+ *
+ * -------------------------------------------------------------------------- */
+
+#include "flox/sqlite.hh"
+#include "flox/exceptions.hh"
+
+
+/* -------------------------------------------------------------------------- */
+
+namespace flox {
+  namespace sqlite {
+
+/* -------------------------------------------------------------------------- */
+
+  static void
+traceSQL( void * x, const char * sql )
+{
+  /* Unrolled from of `nix::notice()'. */
+  if ( nix::lvlNotice <= nix::verbosity )
+    {
+      nix::logger->log( nix::lvlNotice, nix::fmt( "SQL<[%1%]>", sql ) );
+    }
+};
+
+/* -------------------------------------------------------------------------- */
+
+SQLiteDb::SQLiteDb( const std::string & path
+                  ,       int           flags
+                  ,       bool          cache
+                  ,       bool          trace
+                  , const char        * vfs
+                  )
+{
+  int ret = sqlite3_open_v2( path.c_str(), & this->db, flags, vfs );
+  if ( ret != SQLITE_OK )
+    {
+      const char * err = sqlite3_errstr( ret );
+      throw resolve::ResolverException(
+        "cannot open SQLite database '" + path + "': " + std::string( err )
+      );
+    }
+
+  if ( sqlite3_busy_timeout( db, 60 * 60 * 1000 ) != SQLITE_OK )
+    {
+      nix::SQLiteError::throw_( db, "setting timeout" );
+    }
+
+  if ( trace ) { sqlite3_trace( db, & traceSQL, nullptr ); }
+
+  this->exec( "pragma foreign_keys = 1" );
+
+  if ( cache ) { this->isCache(); }
+}
+
+
+/* -------------------------------------------------------------------------- */
+
+  }  /* End Namespace `flox::sqlite' */
+}  /* End Namespace `flox' */
+
+
+/* -------------------------------------------------------------------------- *
+ *
+ *
+ *
+ * ========================================================================== */
