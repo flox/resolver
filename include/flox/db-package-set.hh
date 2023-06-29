102c921e6f99d9333efc096628e53be0da5016aa
Alex Ameen <alex.ameen.tx@gmail.com>
2023-06-29 07:57:36 -0500

    DbPackageSet

--- include/flox/db-package-set.hh
+++ include/flox/db-package-set.hh
@@ -90,68 +90,8 @@ class DbPackageSet : public PackageSet {
 
 /* -------------------------------------------------------------------------- */
 
-    // TODO: change `DrvDb' to accept `std::list<std::string_view'
-      bool
-    hasRelPath( const std::list<std::string_view> & path ) override
-    {
-      std::vector<std::string> rp;
-      if ( this->_stability.has_value() )
-        {
-          rp.emplace_back( this->_stability.value() );
-        }
-      for ( auto & p : path ) { rp.emplace_back( p ); }
-      return this->_db->hasDrv(
-        subtreeTypeToString( this->_subtree )
-      , this->_system
-      , rp
-      ).value_or( false );
-    }
-
-
-/* -------------------------------------------------------------------------- */
-
-      std::size_t
-    size() override
-    {
-      return this->_db->doSQLite( [&]() {
-        auto state = this->getDbState();
-        nix::SQLiteStmt stmt;
-        if ( this->_stability.has_value() )
-          {
-            stmt.create(
-              state->db
-            , "SELECT COUNT( subtree ) FROM DerivationInfos WHERE"
-              "( subtree = ? ) AND ( system = ? ) AND ( path LIKE ? )"
-            );
-            auto query = stmt.use()( subtreeTypeToString( this->_subtree ) )
-                                   ( this->_system )
-                                   ( this->_stability.value() );
-            if ( ! query.next() )
-              {
-                throw ResolverException(
-                  "DbPackageSet::size(): Failed to query table DerivationInfos."
-                );
-              }
-            return query.getInt( 0 );
-          }
-        else
-          {
-            stmt.create( state->db
-                       , "SELECT COUNT( subtree ) FROM DerivationInfos WHERE"
-                         "( subtree = ? ) AND ( system = ? )"
-                       );
-            auto query = stmt.use()( subtreeTypeToString( this->_subtree ) )
-                                   ( this->_system );
-            if ( ! query.next() )
-              {
-                throw ResolverException(
-                  "DbPackageSet::size(): Failed to query table DerivationInfos."
-                );
-              }
-            return query.getInt( 0 );
-          }
-      } );
-    }
+    bool hasRelPath( const std::list<std::string_view> & path ) override;
+    std::size_t size() override;
 
 
 /* -------------------------------------------------------------------------- */
