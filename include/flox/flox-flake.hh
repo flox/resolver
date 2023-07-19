63834a5042fa113f815e096468da9571bbd55697
Alex Ameen <alex.ameen.tx@gmail.com>
2023-07-19 10:27:18 -0500

    kill unused code ( probably pull it in later )

--- include/flox/flox-flake.hh
+++ include/flox/flox-flake.hh
@@ -123,65 +123,12 @@ class FloxFlake : public std::enable_shared_from_this<FloxFlake> {
      * Like `findAttrAlongPath' but without suggestions.
      * Note that each invocation opens the `EvalCache', so use sparingly.
      */
-    Cursor      openCursor(      const std::vector<nix::Symbol>    & path );
-    Cursor      openCursor(      const std::vector<nix::SymbolStr> & path );
-    Cursor      openCursor(      const std::vector<std::string>    & path );
-    MaybeCursor maybeOpenCursor( const std::vector<nix::Symbol>    & path );
-    MaybeCursor maybeOpenCursor( const std::vector<nix::SymbolStr> & path );
-    MaybeCursor maybeOpenCursor( const std::vector<std::string>    & path );
+    Cursor      openCursor(      const std::vector<nix::Symbol> & path );
+    MaybeCursor maybeOpenCursor( const std::vector<nix::Symbol> & path );
 
     /** Opens `EvalCache' once, staying open until all cursors die. */
     std::list<Cursor> getFlakePrefixCursors();
 
-    /**
-     * Determine which output prefixes exist in this flake, ignoring
-     * `preferences' and mark them in the `DrvDb'.
-     * This allows optimized lookups in later attempts to resolve against the
-     * same flake with different preferences.
-     */
-    void recordPrefixes( bool force = false /** Do not optimize out eval. */ );
-
-    /**
-     * Populate this flake's `DrvDb' with paths for all derivations under the
-     * given prefix.
-     * This does NOT populate "info" fields, it only records paths to
-     * all derivations.
-     * This routine is significantly more lightweight than ones focused on
-     * scraping "info" fields making it suitable for quickly resolving
-     * absolute and relative path descriptors.
-     */
-    progress_status populateDerivations( std::string_view subtree
-                                       , std::string_view system
-                                       );
-
-    /**
-     * Apply a function to all derivations with access to an `AttrCursor'.
-     * `nonDrvOp' allows you to control recursive descent into additional
-     * subtrees by appending the `todos' argument passed to `cursor_op'.
-     */
-    progress_status derivationsDo(
-      std::string_view subtree
-    , std::string_view system
-    , progress_status  doneStatus  /** Use `DBPS_FORCE' to avoid skipping. */
-    , derivation_op    drvOp
-    , cursor_op        nonDrvOp   = handleRecurseForDerivations
-    );
-
-    /**
-     * Apply a function to all `Packages' under a prefix.
-     * This abstracts lookups in caches vs. eval, and will implicitly populate
-     * missing cache members unless `allowCache' disables this behavior.
-     * Arbitrary auxilary data can be accessed within `op' by the `aux'
-     * argument ( optional ).
-     */
-    void packagesDo(
-      std::string_view                                         subtree
-    , std::string_view                                         system
-    , std::function<void( std::any * aux, const Package & p )> op
-
-    , std::any * aux        = nullptr
-    , bool       allowCache = true
-    );
 };
 
 
