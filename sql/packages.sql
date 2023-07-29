94b02bc58812f91389e8603ef65d0e8b6913deb9
Alex Ameen <alex.ameen.tx@gmail.com>
2023-07-29 07:50:52 -0500

    compressed db experiment

---
+++ sql/packages.sql
@@ -0,0 +1,70 @@
+-- ========================================================================== --
+--
+--
+--
+-- -------------------------------------------------------------------------- --
+
+CREATE TABLE PackageSets (
+  path  JSON  NOT NULL PRIMARY KEY
+);
+
+
+-- -------------------------------------------------------------------------- --
+
+INSERT INTO PackageSets VALUES
+-- Standard
+  ('["packages","x86_64-linux"]')
+, ('["packages","x86_64-darwin"]')
+, ('["packages","aarch64-linux"]')
+, ('["packages","aarch64-darwin"]')
+-- Legacy
+, ('["legacyPackages","x86_64-linux"]')
+, ('["legacyPackages","x86_64-darwin"]')
+, ('["legacyPackages","aarch64-linux"]')
+, ('["legacyPackages","aarch64-darwin"]')
+-- Catalogs
+, ('["catalog","x86_64-linux","stable"]')
+, ('["catalog","x86_64-linux","staging"]')
+, ('["catalog","x86_64-linux","unstable"]')
+, ('["catalog","x86_64-darwin","stable"]')
+, ('["catalog","x86_64-darwin","staging"]')
+, ('["catalog","x86_64-darwin","unstable"]')
+, ('["catalog","aarch64-linux","stable"]')
+, ('["catalog","aarch64-linux","staging"]')
+, ('["catalog","aarch64-linux","unstable"]')
+, ('["catalog","aarch64-darwin","stable"]')
+, ('["catalog","aarch64-darwin","staging"]')
+, ('["catalog","aarch64-darwin","unstable"]')
+;
+
+
+-- -------------------------------------------------------------------------- --
+
+CREATE VIEW v_PackageSets_RowNums AS
+  SELECT path, row_number() OVER ( ORDER BY path ) AS row_number
+  FROM PackageSets;
+
+
+-- -------------------------------------------------------------------------- --
+
+CREATE VIEW v_PackageSets_PathParts AS SELECT
+    row_number
+  , subtree
+  , system
+  , iif( subtree = 'catalog', json_extract( rest, '$[0]' ), NULL )
+    AS stability
+  , iif( subtree = 'catalog', json_remove( rest, '$[0]' ), rest ) AS rest
+  FROM (
+    SELECT   row_number
+           , json_extract( path, '$[0]' )        AS subtree
+           , json_extract( path, '$[1]' )        AS system
+           , json_remove( path, '$[0]', '$[0]' ) AS rest
+    FROM v_PackageSets_RowNums
+  );
+
+
+-- -------------------------------------------------------------------------- --
+--
+--
+--
+-- ========================================================================== --
